5e213fd3bf2e5d25f12a227a711dfa760ecee8e4
Alexandre Cadeau <alexandre.cadeau@etu.univ-nantes.fr>
2016-05-31 11:48:10 +0200

    Edit Config Angular

--- js/app.js
+++ js/app.js
@@ -8,7 +8,7 @@ const _ = require('lodash'),
 angular.module('room-share', ['ui.router', 'restangular']).constant('_', _)
   .config(['$stateProvider', '$urlRouterProvider', 'RestangularProvider', function($stateProvider, $urlRouterProvider, RestangularProvider){
     RestangularProvider.setBaseUrl('https://api.jcdecaux.com/vls/v1/');
-    RestangularProvider.setRequestSuffix('&apikey=f9d62cfb40a43eb37ebb196f3d80fd24d6a299e7');
+    RestangularProvider.setRequestSuffix('?contract=Nantes&apikey=f9d62cfb40a43eb37ebb196f3d80fd24d6a299e7');
     $urlRouterProvider.otherwise('/home');
     $stateProvider.state('home', {
       url: '/home',
@@ -23,7 +23,12 @@ angular.module('room-share', ['ui.router', 'restangular']).constant('_', _)
               return StationsService.getAll();
             }
           }
+        },
+        'findNearStation@home': {
+          controller: 'NearStationController',
+          templateUrl: 'nearstation.html'
         }
+
       }
     })
     .state('stations', {
@@ -36,7 +41,7 @@ angular.module('room-share', ['ui.router', 'restangular']).constant('_', _)
       templateUrl: 'stationsDetails.html',
       controller: 'StationsDetailsController',
       resolve: {
-        details : function(StationsService, $stateParams){
+        station : function(StationsService, $stateParams){
           return StationsService.getStationDetails($stateParams.StationId);
         }
       }
